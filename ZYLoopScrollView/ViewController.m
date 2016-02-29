97df15319cae18d0e209deeecbec94c7f87c70c3
yi <zhengyi@tinckay.com>
2016-02-29 11:07:46 +0800

    Scrollview 轮播

--- ZYLoopScrollView/ViewController.m
+++ ZYLoopScrollView/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LoopScrollView.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSArray *imageArray = @[
+                            @"http://img03.liwushuo.com/image/160222/cya7ul0v2.jpg-w720",
+                            @"http://img03.liwushuo.com/image/160225/s0zlrxo86.jpg-w720",
+                            @"http://img02.liwushuo.com/image/160204/ql9tvfhax.jpg-w720",
+                            @"http://img01.liwushuo.com/image/160219/1no7rb071.jpg-w720"];
+    
+    LoopScrollView *loopScrollView = [[LoopScrollView alloc] initWithFrame:CGRectMake(0, 20, self.view.bounds.size.width, 200)];
+    //设置属性
+    loopScrollView.pageCount = (int)imageArray.count;
+    loopScrollView.autoScroll = YES;
+    loopScrollView.showPageControl = YES;
+    [loopScrollView setClickAction:^(UIImageView *imageView, int index) {
+        
+        NSLog(@"点击第%d张图",(int)index);
+        
+    }];
+    //设置网络图片
+    //注意: 添加网络图片之前, 需要先设置显示的页数
+    for (int i=0; i<imageArray.count; i++) {
+        [loopScrollView setImageWithUrlString:imageArray[i] atIndex:i];
+    }
+    [self.view addSubview:loopScrollView];
 }
 
 - (void)didReceiveMemoryWarning {
