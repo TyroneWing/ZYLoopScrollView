010da138e1fa534cf078e7b8c6e2ac09494c03c1
yi <zhengyi@tinckay.com>
2016-02-29 10:17:29 +0800

    Initial Commit

---
+++ ZYLoopScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYLoopScrollView
+//
+//  Created by yi on 16/2/29.
+//  Copyright © 2016年 yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYLoopScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYLoopScrollView
+//
+//  Created by yi on 16/2/29.
+//  Copyright © 2016年 yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
